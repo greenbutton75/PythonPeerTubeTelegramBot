1dd2a17daea93b44cd8d4f06004ffec0c23903c7
Valentin Kolesov <vkolesov@rixtrema.com>
2022-02-18 16:07:54 +0700

    More APIs

--- app.py
+++ app.py
@@ -24,19 +24,45 @@ load_dotenv()
 
 API_URL = os.environ.get("API_URL")
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+DEBUG = os.environ.get("DEBUG")
 
 
-SERVERS, DURATION, GOTDURATION = range(3)
-
+API, SERVERS, DURATION, GOTDURATION, GOTSERVERNAME = range(5)
 
 def start(update: Update, context: CallbackContext) -> int:
     """Starts the conversation"""
-    reply_keyboard = [['Mute', 'Unmute', 'Cancel']]
+    # Erase all session info
+    erase_context(context)
+
+    user = update.message.from_user
+    logger.info("%s: %s %s %s", user.first_name, user.last_name, user.username, user.id)
+
+    reply_keyboard = [['Node'], ['NodeProxy'],['Peertube'],['Cancel']]
 
     update.message.reply_text(
         'Peertube UptimeBot '
         'Send /cancel to stop talking to me.\n\n'
         'What do you want to do?',
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder='API'
+        ),
+    )
+
+    return API
+
+def commands(update: Update, context: CallbackContext) -> int:
+    """Select command"""
+    context.user_data["selected_api"] = update.message.text
+
+    if update.message.text=="Cancel":
+        cancel(update,context)
+        return ConversationHandler.END
+        
+    reply_keyboard = [['Mute', 'Unmute'],['Add','Delete'],['Cancel']]
+
+    update.message.reply_text(
+        'Send /cancel to stop talking to me.\n\n'
+        'Select command',
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, input_field_placeholder='Command'
         ),
@@ -55,12 +81,15 @@ def server_list(update: Update, context: CallbackContext) -> int:
         cancel(update,context)
         return ConversationHandler.END
 
+    if selected_command=="Add":
+        update.message.reply_text('Add server',  reply_markup=ReplyKeyboardRemove())
+        return GOTSERVERNAME
 
     try:
         reply_keyboard = []
 
         r = requests.get(
-            API_URL + "/Peertube/List" 
+            API_URL + "/"+context.user_data["selected_api"]+"/List" 
         )
         data = r.json()
         for server in data:
@@ -68,6 +97,8 @@ def server_list(update: Update, context: CallbackContext) -> int:
                 continue
             if selected_command == "Unmute" and not server["mutedTo"]:
                 continue
+            # For Delete command - take all servers
+
             parsed_url = urllib.parse.urlparse(server["url"])
             t = []
             t.append(parsed_url.netloc)
@@ -75,7 +106,7 @@ def server_list(update: Update, context: CallbackContext) -> int:
 
     except Exception as ex:
         logger.info("error: %s", ex.message)
-        return ""
+        return ConversationHandler.END
 
 
 
@@ -85,6 +116,8 @@ def server_list(update: Update, context: CallbackContext) -> int:
             reply_keyboard, one_time_keyboard=True, input_field_placeholder='Server'
         ),
     )
+    if selected_command == "Delete" :
+        return GOTSERVERNAME
 
     return DURATION
 
@@ -106,6 +139,7 @@ def duration(update: Update, context: CallbackContext) -> int:
 
         context.user_data["selected_command"] = ""
         context.user_data["selected_server"] = ""
+        context.user_data["selected_api"] = ""
 
         return ConversationHandler.END
 
@@ -142,16 +176,31 @@ def cancel(update: Update, context: CallbackContext) -> int:
         'Bye!', reply_markup=ReplyKeyboardRemove()
     )
     # Erase all session info
-    context.user_data["selected_command"] = ""
-    context.user_data["selected_server"] = ""
-    context.user_data["selected_duration"] = ""
+    erase_context(context)
+
 
     return ConversationHandler.END
 
-def executecommand(context: CallbackContext):
-    logger.info("EXECUTE COMMAND: %s %s %s", context.user_data["selected_command"], context.user_data["selected_server"], context.user_data["selected_duration"])
 
-    command  = API_URL + "/Peertube/" + context.user_data["selected_command"] + "?url=" + context.user_data["selected_server"]
+def gotservername(update: Update, context: CallbackContext) -> int:
+    """Stores the info about the selected_server."""
+    context.user_data["selected_server"] = update.message.text
+    selected_server = context.user_data["selected_server"]
+    logger.info("selected_server: %s", selected_server)
+
+    response = executecommand(context)
+    update.message.reply_text(
+        response, reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
+def executecommand(context: CallbackContext):
+    msg= f"EXECUTE : {context.user_data['selected_api']}, {context.user_data['selected_command']}, {context.user_data['selected_server']}, {context.user_data['selected_duration']}"
+    logger.info(msg)
+
+    command  = API_URL + "/"+context.user_data["selected_api"]+"/" + context.user_data["selected_command"] + "?url=" + context.user_data["selected_server"]
     if context.user_data["selected_command"] == "Mute":
         t = context.user_data["selected_duration"].split()[0]
         command = command + "&minutes=" + t
@@ -161,6 +210,10 @@ def executecommand(context: CallbackContext):
         'accept': '*/*'
     }
 
+    if DEBUG:
+       return msg 
+
+    # Production
     try:
         response = requests.request("POST", command, headers=headers, data=payload)
     except Exception as ex:
@@ -171,10 +224,15 @@ def executecommand(context: CallbackContext):
         return f'Error: {response.status_code}: {response.text}'
 
     if response.text == "":
-        return "Success"
+        return msg
 
     return response.text
 
+def erase_context(context: CallbackContext):
+    context.user_data["selected_command"] = ""
+    context.user_data["selected_server"] = ""
+    context.user_data["selected_duration"] = ""
+    context.user_data["selected_api"] = ""
 
 def main() -> None:
     """Run the bot."""
@@ -189,9 +247,11 @@ def main() -> None:
         #entry_points=[CommandHandler('start', start)],
         entry_points=[MessageHandler(Filters.text, start)],
         states={
-            SERVERS: [MessageHandler(Filters.regex('^(Mute|Unmute|Cancel)$'), server_list)],
+            API: [MessageHandler(Filters.regex('^(Node|NodeProxy|Peertube|Cancel)$'), commands)],
+            SERVERS: [MessageHandler(Filters.regex('^(Mute|Unmute|Add|Delete|Cancel)$'), server_list)],
             DURATION: [MessageHandler(Filters.text, duration)],
             GOTDURATION: [MessageHandler(Filters.text, gotduration)],
+            GOTSERVERNAME: [MessageHandler(Filters.text, gotservername)]
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
